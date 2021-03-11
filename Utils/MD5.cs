06389899bd57d6a93492fca7621d0cb2824a4b83
曹一梁 <caoyiliang@me.com>
2021-03-11 12:13:50 +0800

    md5 Base64显示

--- Utils/MD5.cs
+++ Utils/MD5.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -8,15 +9,16 @@ namespace Utils
     /// </summary>
     public class MD5
     {
-        public static string GetMD5Hash(string str)
+        public static string GetMD5Hash(string str, bool base64 = true)
         {
             //就是比string往后一直加要好的优化容器
             StringBuilder sb = new StringBuilder();
-            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            using MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            //将输入字符串转换为字节数组并计算哈希。
+            byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+            if (base64) return Convert.ToBase64String(data);
+            else
             {
-                //将输入字符串转换为字节数组并计算哈希。
-                byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
-
                 //X为     十六进制 X都是大写 x都为小写
                 //2为 每次都是两位数
                 //假设有两个数10和26，正常情况十六进制显示0xA、0x1A，这样看起来不整齐，为了好看，可以指定"X2"，这样显示出来就是：0x0A、0x1A。 
@@ -25,14 +27,14 @@ namespace Utils
                 int length = data.Length;
                 for (int i = 0; i < length; i++)
                     sb.Append(data[i].ToString("X2"));
+                return sb.ToString();
             }
-            return sb.ToString();
         }
 
         //验证
-        public static bool VerifyMD5Hash(string str, string hash)
+        public static bool VerifyMD5Hash(string str, string hash, bool base64 = true)
         {
-            string hashOfInput = GetMD5Hash(str);
+            string hashOfInput = GetMD5Hash(str, base64);
             return hashOfInput.CompareTo(hash) == 0;
         }
     }
