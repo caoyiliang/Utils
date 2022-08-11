ef22a26fd8fe8fb4047ba6cc231c25b9fa846103
曹一梁 <caoyiliang@me.com>
2022-08-11 23:36:41 +0800

    优化插件加载方式

--- Utils/PluginHelper.cs
+++ Utils/PluginHelper.cs
@@ -4,9 +4,9 @@ namespace Utils
 {
     public static class PluginHelper
     {
-        public static List<T> GetChildren<T>()
+        public static List<Type> GetChildren<T>()
         {
-            var result = new List<T>();
+            var result = new List<Type>();
             var pluginName = typeof(T).Name;
             var pluginPath = FindPlugin(pluginName);
             foreach (string fileName in pluginPath)
@@ -17,13 +17,16 @@ namespace Utils
                 {
                     if (type.GetInterface(pluginName) != null)
                     {
-                        var instance = Activator.CreateInstance(type);
-                        if (instance is not null)
-                            result.Add((T)instance);
+                        result.Add(type);
                     }
                 }
             }
             return result;
+        }
+
+        public static object CreateInstance(Type type, params object?[]? args)
+        {
+            return Activator.CreateInstance(type, args);
         }
 
         //查找所有插件的路径
