62751509459c0926b2873717ba6593b7c3d5427b
曹一梁 <caoyiliang@me.com>
2024-02-04 14:50:01 +0800

    优化重试

--- Utils/ProcessUtils.cs
+++ Utils/ProcessUtils.cs
@@ -8,25 +8,30 @@ public static class ProcessUtils
     /// 重试
     /// </summary>
     /// <typeparam name="T">返回类型</typeparam>
-    /// <param name="func">需要重试的方法</param>
+    /// <param name="task">需要重试的方法</param>
     /// <param name="reTryCount">需要重试的次数</param>
     /// <param name="cancellationTokenSource">取消</param>
     /// <returns>可空T</returns>
     /// <exception cref="MultipleRetryFailureException">多次重试失败</exception>
     /// <exception cref="ArgumentOutOfRangeException">重试次数超范围</exception>
-    public static async Task<T?> ReTry<T>(Func<Task<T>> func, int reTryCount = 0, CancellationTokenSource? cancellationTokenSource = null)
+    /// <exception cref="TaskCanceledException">任务取消</exception>
+    public static async Task<T?> ReTry<T>(this Task<T> task, int reTryCount = 0, CancellationTokenSource? cancellationTokenSource = null)
     {
         if (reTryCount < 0) throw new ArgumentOutOfRangeException(nameof(reTryCount));
         int j = -1;
-        while (cancellationTokenSource == null || !cancellationTokenSource.IsCancellationRequested)
+        while (true)
         {
+            if (cancellationTokenSource != null && cancellationTokenSource.IsCancellationRequested)
+            {
+                throw new TaskCanceledException();
+            }
             j++;
             if (j > reTryCount) break;
             try
             {
                 T? rsp = default;
-                if (func != null)
-                    rsp = await func();
+                if (task != null)
+                    rsp = await task;
                 return rsp;
             }
             catch (Exception)
@@ -39,23 +44,28 @@ public static class ProcessUtils
     /// <summary>
     /// 重试
     /// </summary>
-    /// <param name="func">需要重试的方法</param>
+    /// <param name="task">需要重试的方法</param>
     /// <param name="reTryCount">需要重试的次数</param>
     /// <param name="cancellationTokenSource">取消</param>
     /// <exception cref="MultipleRetryFailureException">多次重试失败</exception>
     /// <exception cref="ArgumentOutOfRangeException">重试次数超范围</exception>
-    public static async Task ReTry(Func<Task> func, int reTryCount = 0, CancellationTokenSource? cancellationTokenSource = null)
+    /// <exception cref="TaskCanceledException">任务取消</exception>
+    public static async Task ReTry(this Task task, int reTryCount = 0, CancellationTokenSource? cancellationTokenSource = null)
     {
         if (reTryCount < 0) throw new ArgumentOutOfRangeException(nameof(reTryCount));
         int j = -1;
-        while (cancellationTokenSource == null || !cancellationTokenSource.IsCancellationRequested)
+        while (true)
         {
+            if (cancellationTokenSource != null && cancellationTokenSource.IsCancellationRequested)
+            {
+                throw new TaskCanceledException();
+            }
             j++;
             if (j > reTryCount) break;
             try
             {
-                if (func != null)
-                    await func();
+                if (task != null)
+                    await task;
                 return;
             }
             catch (Exception)
